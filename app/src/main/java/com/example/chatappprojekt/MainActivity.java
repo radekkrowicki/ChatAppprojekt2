3ab0deb28c4dcb0bb52c17d00c6ba73bb13c1349
radekkrowicki <radekkrowicki@poczta.fm>
2019-03-26 15:03:40 +0100

    FirebaseAuth nie działa

--- app/src/main/java/com/example/chatappprojekt/MainActivity.java
+++ app/src/main/java/com/example/chatappprojekt/MainActivity.java
@@ -3,11 +3,34 @@ package com.example.chatappprojekt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate() {
+        super.onCreate();
         setContentView(R.layout.activity_main);
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            // Start sign in/sign up activity
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(),
+                    SIGN_IN_REQUEST_CODE
+            );
+        } else {
+            // User is already signed in. Therefore, display
+            // a welcome Toast
+            Toast.makeText(this,
+                    "Welcome " + FirebaseAuth.getInstance()
+                            .getCurrentUser()
+                            .getDisplayName(),
+                    Toast.LENGTH_LONG)
+                    .show();
+
+            // Load chat room contents
+            displayChatMessages();
+        }
     }
 }
